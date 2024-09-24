7c31e011ea2c2e49c8a5e292fabe034f34683a3a
sahilWork <sahilkumar0198@gmail.com>
2024-09-24 12:06:42 +0530

    test

---
+++ app/routes/app.frontend.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import '@shopify/polaris/build/esm/styles.css';
+
+
+const Frontend = () => {
+  return (
+    <>
+      <h1>Frontend</h1>
+      <p>Welcome to your dashboard. Here you can view various metrics and settings.</p>
+      </>
+  );
+};
+
+export default Frontend;
